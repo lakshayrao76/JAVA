d4eb09d5c559a4dedcccaffd788fbda4cbba86d3
lakshayrao76 <24csu112@ncuindia.edu>
2025-11-16 15:55:05 +0530

    Add files via upload

---
+++ EXP7/OutsidePackageDemo.java
@@ -0,0 +1,11 @@
+import exercises.java.PackageDemo;
+
+public class OutsidePackageDemo {
+    public static void main(String[] args) {
+        PackageDemo obj = new PackageDemo();
+        obj.display();
+
+        System.out.println("\nProgram executed by: Lakshay Yadav");
+        System.out.println("Roll No: 24CSU112");
+    }
+}
---
+++ EXP7/PackageDemoDriver.java
@@ -0,0 +1,11 @@
+package exercises.java;
+
+public class PackageDemoDriver {
+    public static void main(String[] args) {
+        PackageDemo obj = new PackageDemo();
+        obj.display();
+
+        System.out.println(" Lakshay Yadav");
+        System.out.println("Roll No: 24CSU112");
+    }
+}
